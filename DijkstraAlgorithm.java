06d91a52aebb60ef57838e4cfc91083680779881
breezedu <breeze.du@gmail.com>
2014-05-01 20:34:17 -0400

    add DijkstraAlgorithm.java

---
+++ DijkstraAlgorithm.java
@@ -0,0 +1,41 @@
+package rosalind;
+/***************
+ * http://rosalind.info/problems/dij/
+ * http://en.wikipedia.org/wiki/Dijkstra's_algorithm
+ * 
+ * Given: A simple directed graph with positive edge weights from 1 to 10^3 and n≤10^3 vertices
+ * in the edge list format. 
+ * Return: An array D[1..n] where D[i] is the length of a shortest path 
+ * from the vertex 1 to the vertex i (D[1]=0). If i is not reachable from 1 set D[i] to −1.
+ * 
+ * Sample DataSet
+ * 6 10
+ * 3 4 4
+ * 1 2 4
+ * 1 3 2
+ * 2 3 3
+ * 6 3 2
+ * 3 5 5
+ * 5 4 1
+ * 3 2 1
+ * 2 4 2
+ * 2 5 3
+ * 
+ * Sample Output
+ * 0 3 2 5 6 -1
+ * 
+ * @author Frog
+ *
+ */
+
+public class DijkstraAlgorithm {
+	
+	public static void main(String[] args){
+		
+		System.out.println("This is Dijkstra's algorithm program.");
+		
+		to be done :)
+		
+	}//end main();
+
+}//end of everything in DijkstraAlgorithm class;
