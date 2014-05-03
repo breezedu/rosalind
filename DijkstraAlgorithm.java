d04fe4bd6e56c105da22a5b866645065ba3be2c3
breezedu <breeze.du@gmail.com>
2014-05-03 15:26:52 -0400

    update DijkstraAlgorithm.java class

--- DijkstraAlgorithm.java
+++ DijkstraAlgorithm.java
@@ -40,18 +40,19 @@ public class DijkstraAlgorithm {
 		
 		System.out.println("This is Dijkstra's algorithm program.");
 		
+		/*********************************************************/
 		//1st of all, read in the num of vertices and edges
 		Scanner readScan = new Scanner(new File("dijk.txt"));
 		
 		int vertices = readScan.nextInt();
 		int edges = readScan.nextInt();
 		
-		int[][] distance = new int[vertices+1][vertices+1];
+		int[][] distMatrix = new int[vertices+1][vertices+1];
 		for(int i=0; i<=vertices; i++){
 			
 			for(int j=0; j<=vertices; j++){
 				
-				distance[i][j] = 99999999;
+				distMatrix[i][j] = 99999999;
 			}
 		}//end for i<=vertices loop;
 		
@@ -59,7 +60,6 @@ public class DijkstraAlgorithm {
 		//declare an array of vert[] to record whether that vertex has been visited or not;
 		int[] vert = new int[vertices+1];
 		ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-		ArrayList<ArrayList<Integer>> ascent = new ArrayList<ArrayList<Integer>>();
 		
 		for(int i=0; i<=vertices; i++){
 			vert[i] = 0; 				//vert[i]=0 means vertex[i] has not been visited;
@@ -67,12 +67,12 @@ public class DijkstraAlgorithm {
 			ArrayList<Integer> empty = new ArrayList<Integer>();
 			graph.add(empty);
 			
-			ArrayList<Integer> ascentEmpty = new ArrayList<Integer>();
-			ascent.add(ascentEmpty);
 			
 		}//end for i<=vertices loop;
 		
 		
+
+		/*********************************************************/
 		//2nd, read in edges;
 		for(int i=0; i<edges; i++){
 			
@@ -81,10 +81,9 @@ public class DijkstraAlgorithm {
 			int dis = readScan.nextInt();
 			
 			graph.get(v1).add(v2);
-			ascent.get(v2).add(v1);		//add v1 to v2's ascent ArrayList;
 			
 			//build the distance matrix:
-			if(dis < distance[v1][v2]) distance[v1][v2] = dis;
+			if(dis < distMatrix[v1][v2]) distMatrix[v1][v2] = dis;
 		//	if(dis < distance[v2][v1]) distance[v2][v1] = dis;
 			
 		}//enf for i<edges loop;
@@ -92,10 +91,14 @@ public class DijkstraAlgorithm {
 		readScan.close(); 	//end readScan, all vertices and edges have been read in;
 		
 		
+
+		/*********************************************************/
 		//3rd, printout the distance matrix to check if everything is ok:
-	//	printMatrix(distance);
+		//	printMatrix(distance);
 		
 		
+
+		/*********************************************************/
 		//4th, dijkstra() the initial vertex;
 		//create an array of distance from initial vertex to vertex[i];
 		int[] dist = new int[vertices+1];
@@ -104,10 +107,12 @@ public class DijkstraAlgorithm {
 		}
 		dist[1] = 0;	//int currDis = 0;
 		
-		dijkstra(1, vert, dist, distance, graph, ascent);
+		dijkstra(1, vert, dist, distMatrix, graph);
 		//	vert[i] = 1;
 		
 		
+
+		/*********************************************************/
 		//5th, printout the distance array;
 		System.out.println("The distance from initial vertex to each vertex are: ");
 		for(int i=1; i<=vertices; i++){
@@ -120,10 +125,46 @@ public class DijkstraAlgorithm {
 			}
 		}//end for i<=vertices loop;
 		
+		
 	}//end main();
 
-	private static void dijkstra(int n, int[] vert, int[] dist, int[][] distance, 
-			ArrayList<ArrayList<Integer>> graph, ArrayList<ArrayList<Integer>> ascent) {
+	/**************
+	 * 1 Assign to every node a tentative distance value: 
+	 * set it to zero for our initial node and to infinity for all other nodes.
+	 * 
+	 * 2 Mark all nodes unvisited. Set the initial node as current. 
+	 * Create a set of the unvisited nodes called the unvisited set consisting of all the nodes.
+	 * 
+	 * 3 For the current node, consider all of its unvisited neighbors and 
+	 * calculate their tentative distances. Compare the newly calculated tentative distance 
+	 * to the current assigned value and assign the smaller one. 
+	 * For example, if the current node A is marked with a distance of 6, 
+	 * and the edge connecting it with a neighbor B has length 2, 
+	 * then the distance to B (through A) will be 6 + 2 = 8. 
+	 * If B was previously marked with a distance greater than 8 then change it to 8. 
+	 * Otherwise, keep the current value.
+	 * 
+	 * 4 When we are done considering all of the neighbors of the current node, 
+	 * mark the current node as visited and remove it from the unvisited set. 
+	 * A visited node will never be checked again.
+	 * 
+	 * 5 If the destination node has been marked visited 
+	 * (when planning a route between two specific nodes) or if the smallest tentative distance 
+	 * among the nodes in the unvisited set is infinity (when planning a complete traversal; 
+	 * occurs when there is no connection between the initial node and remaining unvisited nodes), 
+	 * then stop. The algorithm has finished.
+	 * 
+	 * 6 Select the unvisited node that is marked with the smallest tentative distance, 
+	 * and set it as the new "current node" then go back to step 3.
+	 * 
+	 * @param n		current vertex;
+	 * @param vert	vert[] array, to indicate whether the vertex[i] has been visited or not;
+	 * @param dist	the distance from initial vertex to vert[i];
+	 * @param distMatrix	distance matrix between two vertices;
+	 * @param graph
+	 */
+	private static void dijkstra(int n, int[] vert, int[] dist, int[][] distMatrix, 
+			ArrayList<ArrayList<Integer>> graph) {
 		// TODO Auto-generated method stub
 		if(vert[n] == 1) return;
 		
@@ -133,32 +174,17 @@ public class DijkstraAlgorithm {
 			int v1 = n;
 			int v2 = graph.get(n).get(i);
 			
-			if(distance[v1][v2] + dist[v1] < dist[v2]) {
+			if(distMatrix[v1][v2] + dist[v1] < dist[v2]) {
 				
-				dist[v2] = dist[v1]+distance[n][v2];
+				dist[v2] = dist[v1]+distMatrix[n][v2];
 			
 			}
-			
 			
 		}//end for i<board loop;
 		
 		//mark vert[n] as visited;
-		vert[n] = 1;
+		vert[n] = 1;		
 		
-		
-		//check all vertex n's ascent to vertex[n]'s distance;
-		
-		int ascLen = ascent.get(n).size();
-		for(int i=0; i<ascLen; i++){
-			
-			int v1 = ascent.get(n).get(i);
-			int v2 = n;
-			
-			if(distance[v1][v2] + dist[v1] < dist[v2]){
-				dist[v2] = distance[v1][v2]+dist[v1];
-			}
-			
-		}//end for i<ascLen loop;
 		
 		//printout all distances from initial vertex to vert[n]'s neighbors
 		
@@ -170,14 +196,38 @@ public class DijkstraAlgorithm {
 		
 		
 		//dijkstra() all n's neighbors;
-		for(int i=0; i<broad; i++){
+		
+		//Recursively select the smallest tentative distance vertex, set it as current vertex, 
+		//dijkstra(currentV, vert, dist, distance, graph);
+		int len = vert.length;
+		int min = 999999;
+		int minIndex = -1;
+		for(int i=1; i<len; i++){
 			
-			dijkstra(graph.get(n).get(i), vert, dist, distance, graph, ascent);
-		}
-
+			if(vert[i]==0 && dist[i]<min) {
+				minIndex = i;
+				min = dist[i];
+				
+			}//end if conditions;
+			
+		}//end for i<len loop; the minimum vertex has been checked out.
+		
+		//if minIndex = -1 after the upper loop, that means the dijkstra() method will stop;
+		if(minIndex!= -1){
+			
+			dijkstra(minIndex, vert, dist, distMatrix, graph);
+		} else {
+			
+			System.out.println("dijkstra method stop here;");
+		}//end if-else conditions;
 		
 	}//end dijkstra() method;
 
+	
+	/************
+	 * Printout the distance matrix;
+	 * @param matrix
+	 *
 	private static void printMatrix(int[][] matrix) {
 		// TODO Printout a matrix
 		
@@ -213,5 +263,6 @@ public class DijkstraAlgorithm {
 		}//end outer for i<row loop;
 		
 	}//end printMatrix() method;
-
+	*/
+	
 }//end of everything in DijkstraAlgorithm class;
