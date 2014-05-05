05ada12c71aa88c4aa81236ca0ebdc3a2d5e8f64
breezedu <breeze.du@gmail.com>
2014-05-05 12:22:02 -0400

    add BuildingaHeap.java class

---
+++ BuildingaHeap.java
@@ -0,0 +1,27 @@
+package rosalind;
+/**************
+ * http://rosalind.info/problems/hea/
+ * http://en.wikipedia.org/wiki/Heap_(data_structure)
+ * 
+ * Given: A positive integer n≤105 and array A[1..n] of integers from −105 to 105.
+ * Return: A permuted array A satisfying the binary max heap property: for any 2≤i≤n, A[⌊i/2⌋]≥A[i].
+ * 
+ * Sample DataSet
+ * 5
+ * 1 3 5 7 2
+ * 
+ * Sample Output
+ * 7 5 1 3 2
+ * 
+ * @author Yan
+ *
+ */
+public class BuildingaHeap {
+	
+	public static void main(String[] args){
+		
+		System.out.println("This is Building a Heap program.");
+		
+	}//end main();
+
+}//end of everything in BuildingaHeap class
